cce91049bd1e1c67fd120c179e7578ec6c4d4319
Rik van der Bolt <rik.vd.bolt@gmail.com>
2020-05-22 15:04:05 +0200

    weapon reloading
    1

--- Project_Titan/Assets/Scripts/GunHandler.cs
+++ Project_Titan/Assets/Scripts/GunHandler.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class GunHandler : MonoBehaviour
 {
@@ -6,14 +7,36 @@ public class GunHandler : MonoBehaviour
     public int range = 100;
     public float fireRate = 15f;
 
+    public int maxAmmo = 30;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public AudioSource fire;
     public Camera cam;
 
     private float nextTimeToFire = 0f;
 
-  
+    public Animator animator;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+
     void Update()
     {
+
+        if (isReloading)
+            return;
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (Input.GetButton ("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
@@ -23,6 +46,9 @@ public class GunHandler : MonoBehaviour
 
     void Shoot()
     {
+
+        currentAmmo--;
+
         fire.Play();
         RaycastHit hit;
         if (Physics.Raycast (cam.transform.position, cam.transform.forward, out hit, range))
@@ -40,6 +66,23 @@ public class GunHandler : MonoBehaviour
                 target.TakeDamage(damage); 
             }
         }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("kogels zijn op");
+
+        animator.SetBool("reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+
+        animator.SetBool("reloading", false);
+
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 
 }
