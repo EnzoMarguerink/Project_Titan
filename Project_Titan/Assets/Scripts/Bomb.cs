bdfecb8595d44d6317c61666e860c4185b24ec89
Rik van der Bolt <rik.vd.bolt@gmail.com>
2020-05-27 18:26:28 +0200

    assets
    1700 Pog

---
+++ Project_Titan/Assets/Scripts/Bomb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float range = 1f;
+    public bool m_Start;
+    public Transform player;
+    
+
+    void playerCollision()
+    {
+        if (player.transform.position = )
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (m_Start)
+            Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
